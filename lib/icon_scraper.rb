9711a89b039881b393846850c8ab56fdbc846e6f
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 16:07:23 +1000

    Add swan

--- lib/icon_scraper.rb
+++ lib/icon_scraper.rb
@@ -22,6 +22,11 @@ module IconScraper
         false,
         agent
       )
+    elsif authority == :swan
+      IconScraper.rest_xml(
+        "https://elodge.swan.wa.gov.au/Pages/XC.Track/SearchApplication.aspx",
+        "d=thisweek&k=LodgementDate&t=282,281,283&o=xml"
+      )
     else
       raise "Unexpected authority: #{authority}"
     end
