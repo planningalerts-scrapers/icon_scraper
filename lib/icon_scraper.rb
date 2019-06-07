45d4b6a8b8dcbcbfd6c2bfe3b71f3e8c4de8fe5c
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 15:44:59 +1000

    Put knowledge of settings in the gem now

--- lib/icon_scraper.rb
+++ lib/icon_scraper.rb
@@ -4,6 +4,29 @@ require "mechanize"
 require "scraperwiki"
 
 module IconScraper
+  def self.scrape_and_save(authority)
+    if authority == :blue_mountains
+      starting_url = "https://www2.bmcc.nsw.gov.au/DATracking/Pages/XC.Track/SearchApplication.aspx"
+
+      agent = Mechanize.new
+
+      doc = agent.get(starting_url)
+      form = doc.forms.first
+      button = form.button_with(value: "I Agree")
+      raise "Can't find agree button" if button.nil?
+      doc = form.submit(button)
+
+      rest_xml(
+        starting_url,
+        "d=last14days&k=LodgementDate&o=xml",
+        false,
+        agent
+      )
+    else
+      raise "Unexpected authority: #{authority}"
+    end
+  end
+
   # Copied from lib_icon_rest_xml repo
   def self.rest_xml(base_url, query, debug = false, agent = nil)
     agent = Mechanize.new unless agent
